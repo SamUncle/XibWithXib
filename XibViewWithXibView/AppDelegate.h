85b97886943d476be090af3083fc008661d23e00
朱守明 <507772308@qq.com>
2018-11-25 17:42:42 +0800

    Initial Commit

---
+++ XibViewWithXibView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XibViewWithXibView
+//
+//  Created by zsm on 2018/11/25.
+//  Copyright © 2018年 XinHuoKeJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XibViewWithXibView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XibViewWithXibView
+//
+//  Created by zsm on 2018/11/25.
+//  Copyright © 2018年 XinHuoKeJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
